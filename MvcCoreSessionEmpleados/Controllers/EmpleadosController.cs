a44eccf55f97b935d0891bb6a03479ded6927a59
serraguti <pacoserranox@gmail.com>
2026-02-25 10:00:35 +0100

    Add project files.

---
+++ MvcCoreSessionEmpleados/Controllers/EmpleadosController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreSessionEmpleados.Repositories;
+
+namespace MvcCoreSessionEmpleados.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private RepositoryEmpleados repo;
+
+        public EmpleadosController(RepositoryEmpleados repo)
+        {
+            this.repo = repo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
